ab9ce225d300f199573c75063f3b01d1e0d9b065
coder161067 <emailtoowen@proton.me>
2026-05-26 15:48:14 +1000

    Server networking issue fix

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ const logger = pino({
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("trust proxy", 1);
+
 app.use(helmet({
   crossOriginResourcePolicy: { policy: "same-origin" },
   contentSecurityPolicy: {
